5ac64e74156ca6dd4520d9bb80e8aabdf612486a
Radoslav Gerganov <rgerganov@gmail.com>
2021-07-29 09:22:11 +0300

    Update for coredns 1.7

--- scripts/setupdns.sh
+++ scripts/setupdns.sh
@@ -3,27 +3,32 @@ kubectl apply -f - <<EOF
 apiVersion: v1
 kind: ConfigMap
 metadata:
-  name: coredns
+  name: coredns-corefile
   namespace: kube-system
 data:
   Corefile: |
     .:53 {
         errors
-        health
+        health {
+          lameduck 5s
+        }
         ready
         kubernetes cluster.local in-addr.arpa ip6.arpa {
-           pods insecure
-           upstream
-           fallthrough in-addr.arpa ip6.arpa
+          pods insecure
+          fallthrough in-addr.arpa ip6.arpa
+          ttl 30
         }
         prometheus :9153
-        forward . /etc/resolv.conf
+        forward . /etc/resolv.conf {
+          max_concurrent 1000
+        }
         cache 30
         loop
         reload
         loadbalance
     }
     global:53 {
+        log
         errors
         cache 30
         forward . $(kubectl get svc -n istio-system istiocoredns -o jsonpath={.spec.clusterIP}):53
